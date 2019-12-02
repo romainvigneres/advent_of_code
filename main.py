2d59c5c57e29aec1f8a3c9ffbd7848f331e4f0ae
romain.vigneres <romain.vigneres@texa.fr>
2019-12-02 11:16:05 +0100

    y17d02

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from year2017.day01.code import get_result as y17d01
+from year2017.day02.code import get_result as y17d02
+
 from year2019.day01.code import get_result as y19d01
 from year2019.day02.code import get_result as y19d02
 
-y17d01()
-y19d01()
-y19d02()
+
+# y17d01()
+y17d02()
+# y19d01()
+# y19d02()
