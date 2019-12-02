b0e202440c0cdb23e2b9e8a0685b4e39b60651ab
romainvig <vigneres.romain@gmail.com>
2019-12-02 20:30:10 +0100

    y17d06

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 # from year2017.day01.code import get_result as y17d01
 # from year2017.day02.code import get_result as y17d02
 # from year2017.day03.code import get_result as y17d03
-from year2017.day04.code import get_result as y17d04
+# from year2017.day04.code import get_result as y17d04
+# from year2017.day05.code import get_result as y17d05
+from year2017.day06.code import get_result as y17d06
 
 # from year2019.day01.code import get_result as y19d01
 # from year2019.day02.code import get_result as y19d02
@@ -10,6 +12,9 @@ from year2017.day04.code import get_result as y17d04
 # y17d01()
 # y17d02()
 # y17d03()
-y17d04()
+# y17d04()
+# y17d05()
+y17d06()
+
 # y19d01()
 # y19d02()
