f435fa35d251f57dd5feec0719070a1becb50c19
romainvig <vigneres.romain@gmail.com>
2019-12-22 10:16:37 +0100

    clean up

--- year2019/day02/code.py
+++ year2019/day02/code.py
@@ -2,22 +2,25 @@ from common import input_integer_sep
 from year2019.intcode import intcode_computer_v0 as intcode_computer
 
 
+def part_one(program):
+    return intcode_computer(program, [12, 2])
+
+
 def test_one():
     assert intcode_computer([1, 0, 0, 0, 99]) == 2
     assert intcode_computer([2, 3, 0, 3, 99]) == 2
     assert intcode_computer([1, 1, 1, 4, 99, 5, 6, 0, 99]) == 30
 
 
-def get_result():
-    program = input_integer_sep("2019", "02")
-    test_one()
-    print("Part one", intcode_computer(program, [12, 2]))
-    exit = False
+def part_two(program):
     for noun in range(0, 100):
         for verb in range(0, 100):
             if intcode_computer(program, [noun, verb]) == 19690720:
-                print("Part two", 100 * noun + verb)
-                exit = True
-                break
-        if exit:
-            break
+                return 100 * noun + verb
+
+
+def get_result():
+    inp = input_integer_sep("2019", "02")
+    test_one()
+    print("Part one", part_one(inp))
+    print("Part two", part_two(inp))
